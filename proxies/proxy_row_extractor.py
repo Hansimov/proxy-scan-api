5178cf44103a793b130fe081e894404c71da49ef
hansimov <591172499@qq.com>
2023-12-12 09:30:07 +0000

    :gem: [Feature] JSCodesConverter: Transform repeat() to ES5

--- proxies/proxy_row_extractor.py
+++ proxies/proxy_row_extractor.py
@@ -1,8 +1,49 @@
 import cssutils
+import js2py
 import re
 
 from bs4 import BeautifulSoup
 from pprint import pprint
+from js2py.internals import seval
+
+
+class JSCodesConverter:
+    def convert(self, js_codes):
+        def transform_repeat(js_codes):
+            pattern = "\.repeat"
+            repeat_str_starts = [
+                match.start() for match in re.finditer(pattern, js_codes)
+            ]
+            repeat_str_pattern = '^"(.+?)"'
+            repeat_times_pattern = '\.repeat\((\d+)\)'
+            new_js_codes = js_codes
+            for i in repeat_str_starts:
+                str_before = js_codes[:i]
+                str_after = js_codes[i:]
+
+                # print(str_before)
+                reversed_str_before = "".join(reversed(str_before))
+                # print(reversed_str_before)
+                repeat_str_match = re.search(repeat_str_pattern, reversed_str_before)
+                repeat_str = "".join(reversed(repeat_str_match.group(1)))
+                # print(repeat_str)
+                repeat_times_match = re.search(repeat_times_pattern, str_after)
+                repeat_times = int(repeat_times_match.group(1))
+                # print(str_after)
+                # print(repeat_times)
+
+                repeat_str_and_times = f'"{repeat_str}".repeat({repeat_times})'
+                print(repeat_str_and_times)
+                array_join_str_and_time = f'Array({repeat_times+1}).join("{repeat_str}")'
+                print(array_join_str_and_time)
+                new_js_codes = new_js_codes.replace(repeat_str_and_times, array_join_str_and_time)
+            print(js_codes)
+            print(new_js_codes)
+            return new_js_codes
+
+        new_js_codes = transform_repeat(js_codes)
+
+        return new_js_codes
 
 
 class ProxyRowExtractor:
@@ -20,13 +61,21 @@ class ProxyRowExtractor:
 
     def extract(self, html_str):
         soup = BeautifulSoup(html_str, "html.parser")
-        rows = soup.find("table").find_all("tr")
+        rows = soup.find("table").find("tbody").find_all("tr")
         for row in rows:
             row_dict = {}
             cells = row.find_all("td")
             for key, cell in zip(self.keys, cells):
                 cell_text = re.sub(r"\s+", " ", cell.text.strip())
-                if key == "bandwidth_and_latency":
+                if key == "ip":
+                    masked_script = cell.find("script").text
+                    js_codes = re.sub("document.write", "console.log", masked_script)
+                    converter = JSCodesConverter()
+                    new_js_codes = converter.convert(js_codes)
+                    ip = js2py.eval_js(new_js_codes)
+                    row_dict["ip"] = ip
+                    break
+                elif key == "bandwidth_and_latency":
                     progress_bar = cell.find("div", class_="progress-bar-inner")
                     bandwidth = cssutils.parseStyle(progress_bar["style"])["width"]
                     latency = cell_text
@@ -46,7 +95,7 @@ class ProxyRowExtractor:
                 else:
                     row_dict[key] = cell_text
             if row_dict:
-                pprint(row_dict)
+                # pprint(row_dict)
                 self.row_dicts.append(row_dict)
         # pprint(row_dicts)
         return self.row_dicts
