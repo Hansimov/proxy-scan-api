71a06e546b03d6d1dc48281f6ebbbc7d44625c04
hansimov <591172499@qq.com>
2023-12-14 11:21:17 +0000

    :gem: [Feature] Scheduler: Enable /scan as background task

--- apis/proxy_scheduler_api.py
+++ apis/proxy_scheduler_api.py
@@ -2,15 +2,18 @@ import argparse
 import json
 import pandas as pd
 import sys
+import time
 import uvicorn
 
-from fastapi import FastAPI, Body
+from fastapi import FastAPI, Body, BackgroundTasks
 from pathlib import Path
 from pydantic import BaseModel, Field
 from typing import Optional
+
+from apis import get_host_port
 from databases import ProxyDatabase
 from utils.logger import logger
-from apis import get_host_port
+from proxies import ProxyScanner
 
 
 class ProxySchedulerAPIApp:
@@ -21,18 +24,26 @@ class ProxySchedulerAPIApp:
             swagger_ui_parameters={"defaultModelsExpandDepth": -1},
             version="1.0",
         )
+        self.scanner = ProxyScanner()
         self.setup_host_port()
         self.setup_routes()
 
     def setup_host_port(self):
         self.host, self.port = get_host_port("proxy-scheduler")
 
+    async def scan(self, background_tasks: BackgroundTasks):
+        background_tasks.add_task(self.scanner.run)
+        return {"status": "success"}
+
     def setup_routes(self):
-        pass
+        self.app.post(
+            "/scan",
+            summary="Scan IPs",
+        )(self.scan)
 
 
 api = ProxySchedulerAPIApp()
 app = api.app
 
 if __name__ == "__main__":
-    uvicorn.run("__main__:app", host=api.host, port=api.port, reload=False)
+    uvicorn.run("__main__:app", host=api.host, port=api.port, reload=True)
